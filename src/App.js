0a82a032d30b98ec5f1fed4c8e54187f77af6858
samssjubair <samssjubair@gmail.com>
2021-02-28 03:20:47 +0600

    btn added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Celebrity from './Components/Celebrity/Celebrity';
 
+
 function App() {
   const [addedCelebrities,setAddedCelebrities]= useState([]);
   function handleClick(celebrity){
--- src/Components/Celebrity/Celebrity.js
+++ src/Components/Celebrity/Celebrity.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './Celebrity.css'
+import './Celebrity.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Celebrity = (props) => {
     const {name,img,phone,username,email,earning}=props.celebrity;
     return (
         <div className="celebrity">
             <img className="img" src={img} alt=""/>
-            <h1>{name}</h1>
-            <h3>{username}</h3>
-            <h3>{email}</h3>
-            <h4>{phone}</h4>
-            <h4>{earning}</h4>
-            <button onClick={()=>props.handleClick(props.celebrity)}>Add Friend</button>
+            <h2>{name}</h2>
+            <h5>@{username}</h5>
+            <h5>{email}</h5>
+            <h5>Mob: {phone}</h5>
+            <h5>Earning: {earning}</h5>
+            <button onClick={()=>props.handleClick(props.celebrity)}><FontAwesomeIcon icon={faPlus} /></button>
         </div>
     );
 };
